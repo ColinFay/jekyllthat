fd1f688dfc8e15981285d75cf41b3d415bcbc121
Colin <colin@thinkr.fr>
2017-12-21 11:42:03 +0100

    pandoc version

--- R/jekylldown.R
+++ R/jekylldown.R
@@ -1,16 +1,22 @@
+pandoc2.0 <- getFromNamespace("pandoc2.0", "rmarkdown")
+
 #' jekylldown
-#' 
+#'
 #' A markdown output for Jekyll Github
-#' 
+#'
 #' @importFrom rmarkdown md_document
-#' 
+#'
 #' @export
-#' 
+#'
 
 jekylldown <- function() {
-  md_document(variant = "gfm", preserve_yaml = TRUE,
+  pandoc2 <- pandoc2.0()
+  variant <- if (pandoc2) "gfm" else "markdown_github"
+  md_document(variant = variant, preserve_yaml = TRUE,
               toc = FALSE, toc_depth = 3, fig_width = 7, fig_height = 5,
               fig_retina = NULL, dev = "png", df_print = "default", includes = NULL,
               md_extensions = NULL, pandoc_args = NULL)
-  
+
 }
+
+
