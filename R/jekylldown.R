4152ed29287b9c95f216b75b32991f21c7e030a6
Colin <colin@thinkr.fr>
2017-12-21 11:44:02 +0100

    patch

--- R/jekylldown.R
+++ R/jekylldown.R
@@ -10,8 +10,11 @@ pandoc2.0 <- getFromNamespace("pandoc2.0", "rmarkdown")
 #'
 
 jekylldown <- function() {
+  # From rmarkdow::github_document
   pandoc2 <- pandoc2.0()
   variant <- if (pandoc2) "gfm" else "markdown_github"
+  variant <- paste0(variant, "-ascii_identifiers")
+
   md_document(variant = variant, preserve_yaml = TRUE,
               toc = FALSE, toc_depth = 3, fig_width = 7, fig_height = 5,
               fig_retina = NULL, dev = "png", df_print = "default", includes = NULL,
